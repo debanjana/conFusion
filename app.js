16106c313b38bc532e8b0b35f057009814e97ef6
Debanjana Maitra <debanjana.maitra@siliconexpert.com>
2020-05-20 23:21:32 -0400

    authentication using passport local - salt key and hashed password

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ var logger = require('morgan');
 var session = require('express-session');
 var FileStore = require('session-file-store')(session);
 
+//passsport
+var passport = require('passport');
+var authenticate = require('./authenticate')
+
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const dishRouter = require('./routes/dishRoute');
@@ -20,7 +25,7 @@ const url = 'mongodb://localhost:27017/conFusion';
 const connect = mongoose.connect(url);
 
 connect.then((db) => {
-    console.log("Connected correctly to server");
+  console.log("Connected correctly to server");
 }, (err) => { console.log(err); });
 
 
@@ -42,7 +47,49 @@ app.use(session({
   store: new FileStore()
 }));
 
+
+/* explanation for passport usage:
+f the user is logged in,
+then what happens is that when the session is initiated again,
+you recall that when you log in here,
+you will be logging in here,
+and a call to the passport authenticate local,
+when this is done at the login stage,
+the passport authenticate local will
+automatically add the user property to the request message.
+So, it'll add req.user and then,
+the passport session that we have done here will automatically
+serialize that user information and then store it in the session.
+So, and subsequently, whenever
+a incoming request comes in from the client side
+with the session cookie already in place,
+then this will automatically load the req.user onto the incoming request.
+So, that is how the passport session itself is organized.
+*/
+
+// auth using passport
+app.use(passport.initialize());
+app.use(passport.session());
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+function auth(req, res, next) {
+  console.log(req.user);
+
+  if (!req.user) {
+    var err = new Error('You are not authenticated!');
+    err.status = 403;
+    next(err);
+  }
+  else {
+    next();
+  }
+}
+
+
 // use user route to signup and logout a user 
+// cookie and session logic
+/*
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
@@ -65,93 +112,99 @@ else {
   }
 }
 }
+*/
 
 
+// cookie logic 
 
-// app.use(cookieParser('12345-67890-09876-54321'));
+/*app.use(cookieParser('12345-67890-09876-54321'));
 
-// function auth (req, res, next) {
+function auth (req, res, next) {
 
-//   if (!req.signedCookies.user) { // first time when no cookie exists
-//     var authHeader = req.headers.authorization;
-//     if (!authHeader) {
-//         var err = new Error('You are not authenticated!');
-//         res.setHeader('WWW-Authenticate', 'Basic');              
-//         err.status = 401;
-//         next(err);
-//         return;
-//     }
-//     var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-//     var user = auth[0];
-//     var pass = auth[1];
-//     if (user == 'admin' && pass == 'password') {
-//         res.cookie('user','admin',{signed: true});
-//         next(); // authorized
-//     } else {
-//         var err = new Error('You are not authenticated!');
-//         res.setHeader('WWW-Authenticate', 'Basic');              
-//         err.status = 401;
-//         next(err);
-//     }
-//   }
-//   else {
-//       if (req.signedCookies.user === 'admin') { // when cookie exists
-//           next();
-//       }
-//       else {
-//           var err = new Error('You are not authenticated!');
-//           err.status = 401;
-//           next(err);
-//       }
-//   }
-// }
+  if (!req.signedCookies.user) { // first time when no cookie exists
+    var authHeader = req.headers.authorization;
+    if (!authHeader) {
+        var err = new Error('You are not authenticated!');
+        res.setHeader('WWW-Authenticate', 'Basic');              
+        err.status = 401;
+        next(err);
+        return;
+    }
+    var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+    var user = auth[0];
+    var pass = auth[1];
+    if (user == 'admin' && pass == 'password') {
+        res.cookie('user','admin',{signed: true});
+        next(); // authorized
+    } else {
+        var err = new Error('You are not authenticated!');
+        res.setHeader('WWW-Authenticate', 'Basic');              
+        err.status = 401;
+        next(err);
+    }
+  }
+  else {
+      if (req.signedCookies.user === 'admin') { // when cookie exists
+          next();
+      }
+      else {
+          var err = new Error('You are not authenticated!');
+          err.status = 401;
+          next(err);
+      }
+  }
+}
 
+*/
 
+// basic auth logic
 
-
-// app.use('/', indexRouter);
-// app.use('/users', usersRouter);
+/*
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 
   
-//   function auth (req, res, next) {
-//       console.log(req.session);
+  function auth (req, res, next) {
+      console.log(req.session);
   
-//       if (!req.session.user) {
-//           var authHeader = req.headers.authorization;
-//           if (!authHeader) {
-//               var err = new Error('You are not authenticated!');
-//               res.setHeader('WWW-Authenticate', 'Basic');                        
-//               err.status = 401;
-//               next(err);
-//               return;
-//           }
-//           var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-//           var user = auth[0];
-//           var pass = auth[1];
-//           if (user == 'admin' && pass == 'password') {
-//               req.session.user = 'admin';
-//               next(); // authorized
-//           } else {
-//               var err = new Error('You are not authenticated!');
-//               res.setHeader('WWW-Authenticate', 'Basic');
-//               err.status = 401;
-//               next(err);
-//           }
-//       }
-//       else {
-//           if (req.session.user === 'admin') {
-//               console.log('req.session: ',req.session);
-//               next();
-//           }
-//           else {
-//               var err = new Error('You are not authenticated!');
-//               err.status = 401;
-//               next(err);
-//           }
-//       }
-//   }
-  app.use(auth);
+      if (!req.session.user) {
+          var authHeader = req.headers.authorization;
+          if (!authHeader) {
+              var err = new Error('You are not authenticated!');
+              res.setHeader('WWW-Authenticate', 'Basic');                        
+              err.status = 401;
+              next(err);
+              return;
+          }
+          var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+          var user = auth[0];
+          var pass = auth[1];
+          if (user == 'admin' && pass == 'password') {
+              req.session.user = 'admin';
+              next(); // authorized
+          } else {
+              var err = new Error('You are not authenticated!');
+              res.setHeader('WWW-Authenticate', 'Basic');
+              err.status = 401;
+              next(err);
+          }
+      }
+      else {
+          if (req.session.user === 'admin') {
+              console.log('req.session: ',req.session);
+              next();
+          }
+          else {
+              var err = new Error('You are not authenticated!');
+              err.status = 401;
+              next(err);
+          }
+      }
+  }
+
+  */
+app.use(auth);
 
 
 app.use(express.static(path.join(__dirname, 'public')));
@@ -162,12 +215,12 @@ app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- models/user.js
+++ models/user.js
@@ -1,20 +1,49 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+// passport 
+var passportLocalMongoose = require('passport-local-mongoose');
+
+// username and password will be added by the plugin
+/* using passport this is how the user is saved in db
+So, you can see that in the database itself,
+the password is not directly stored inside
+the hashed value of the password which is hashed using this salt key here, 
+no password is saved
+salt and hash is used 
+
+	"_id" : ObjectId("5ec5f0999038a4a0a8743b5b"),
+	"admin" : false,
+	"username" : "deb",
+	"salt" : "1c746218c0c3bf28f72a4........1af", // no password
+	"hash" : "472742374029.........",
+    "__v" : 0
+    
+    */
+
 var User = new Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password:  {
-        type: String,
-        required: true
-    },
-    admin:   {
-        type: Boolean,
-        default: false
-    }
+  admin:   {
+      type: Boolean,
+      default: false
+  }
 });
+
+// var User = new Schema({
+//     username: {
+//         type: String,
+//         required: true,
+//         unique: true
+//     },
+//     password:  {
+//         type: String,
+//         required: true
+//     },
+//     admin:   {
+//         type: Boolean,
+//         default: false
+//     }
+// });
+
+User.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model('User', User);
